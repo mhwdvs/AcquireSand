3b669956d601a7f49449cbf2a5aecd1c829f2afe
Matthew Davis <matt@mhwdvs.com>
2021-08-28 17:20:25 +0800

    Change http client, fix URL formatting, add delay to ebay api calls

--- containers/ebayapi/index.js
+++ containers/ebayapi/index.js
@@ -7,7 +7,14 @@ pool.on('error', (err, client) => {
   process.exit(-1);
 });
 
+// got http client
+const got = require('got');
+
 loop();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // database update loop
 async function loop() {
@@ -17,7 +24,8 @@ async function loop() {
     } catch (e) {
       console.error(e);
     }
-    sleep(5);
+    console.log('Sleeping for 60 minutes!');
+    await sleep(60 * 1000);
   }
 }
 
@@ -27,7 +35,6 @@ async function main() {
   // Getting GPU list from database
   try {
     gpuList = await getGPUList();
-    console.log('final initial gpulist: ' + gpuList);
   } catch (e) {
     console.error(
         'Error in getting GPU list from database' + e.name + e.message);
@@ -126,66 +133,72 @@ async function getGPU(gpu) {
   // make request to ebay api
   // Create WebSocket connection.
   const url = [
-    'https://svcs.ebay.com/services/search/FindingService/v1?',
-    'OPERATION-NAME=findItemsAdvanced&', 'SERVICE-VERSION=1.13.0&',
-    'SECURITY-APPNAME=', process.env('EBAY_API_KEY'), '&',
-    'RESPONSE-DATA-FORMAT=JSON&', 'REST-PAYLOAD=true&',
-    'GLOBAL-ID=EBAY-' + COUNTRY + '&',
-    'affiliate.networkId=9&',            // SHOULD BE VARIABLE
-    'affiliate.trackingId=5338664158&',  // SHOULD BE VARIABLE
+    'https://svcs.ebay.com/services/search/FindingService/v1?' +
+    'OPERATION-NAME=findItemsAdvanced&' +
+    'SERVICE-VERSION=1.13.0&' +
+    'SECURITY-APPNAME=' + process.env.EBAY_API_KEY + '&' +
+    'RESPONSE-DATA-FORMAT=JSON&' +
+    'REST-PAYLOAD=true&' +
+    'GLOBAL-ID=EBAY-' + COUNTRY + '&' +
+    'affiliate.networkId=9&' +            // SHOULD BE VARIABLE
+    'affiliate.trackingId=5338664158&' +  // SHOULD BE VARIABLE
     // ITEM FILTERS
     // additional item filters can be found at
     // https://developer.ebay.com/devzone/finding/callref/types/ItemFilterType.html
-    'itemFilter(0).name=Condition&',
-    'itemFilter(0).value(0)=3000&',  // Used
-    'itemFilter(0).value(1)=1000&',  // New
-    'itemFilter(0).value(2)=1500&',  // New (other, see item details
-    'itemFilter(0).value(3)=2000&',  // Manufacturer refurbished
-    'itemFilter(0).value(4)=4000&',  // Very good
-    'itemFilter(0).value(5)=5000&',  // Good
-    'itemFilter(0).value(6)=6000&',  // Acceptable
-    'itemFilter(1).name=ListingType&', 'itemFilter(1).value(0)=FixedPrice&',
-    'itemFilter(1).value(1)=AuctionWithBIN&',
-    'itemFilter(1).value(2)=StoreInventory&',
-    'itemFilter(2).name=LocalPickupOnly&', 'itemFilter(2).value=false&',
-    'itemFilter(3).name=Currency&',
-    'itemFilter(3).value=' + CURRENCY + '&',  // LOCALISATION TARGET
-    'itemFilter(4).name=LocatedIn&',
-    'itemFilter(4).value=' + COUNTRY + '&',  // LOCALISATION TARGET
-    'keywords=',
-    gpu,  // The gpu  being searched for
-    '+-cooler+-performance+-like+-backplate+-waterblock+-buying+-bracket+-fan+-fans+-replacement+-mosfet+-powerlink+-bios+-nvlink+-kit+-suits+-faulty+-1080p+-description&',  // Blacklisted terms
-    'LH_PrefLoc=1&',  // Only show items within region selected
-    'categoryId=27386&',
-    'paginationInput.entriesPerPage=100&',  // redundent?
+    'itemFilter(0).name=Condition&' +
+    'itemFilter(0).value(0)=3000&' +  // Used
+    'itemFilter(0).value(1)=1000&' +  // New
+    'itemFilter(0).value(2)=1500&' +  // New (other, see item details
+    'itemFilter(0).value(3)=2000&' +  // Manufacturer refurbished
+    'itemFilter(0).value(4)=4000&' +  // Very good
+    'itemFilter(0).value(5)=5000&' +  // Good
+    'itemFilter(0).value(6)=6000&' +  // Acceptable
+    'itemFilter(1).name=ListingType&' +
+    'itemFilter(1).value(0)=FixedPrice&' +
+    'itemFilter(1).value(1)=AuctionWithBIN&' +
+    'itemFilter(1).value(2)=StoreInventory&' +
+    'itemFilter(2).name=LocalPickupOnly&' +
+    'itemFilter(2).value=false&' +
+    'itemFilter(3).name=Currency&' +
+    'itemFilter(3).value=' + CURRENCY + '&' +  // LOCALISATION TARGET
+    'itemFilter(4).name=LocatedIn&' +
+    'itemFilter(4).value=' + COUNTRY + '&' +  // LOCALISATION TARGET
+    'keywords=' + gpu +                       // The gpu  being searched for
+    '+-cooler+-performance+-like+-backplate+-waterblock+-buying+-bracket+-fan+-fans+-replacement+-mosfet+-powerlink+-bios+-nvlink+-kit+-suits+-faulty+-1080p+-description&' +  // Blacklisted terms
+    'LH_PrefLoc=1&' +  // Only show items within region selected
+    'categoryId=27386&' +
+    'paginationInput.entriesPerPage=100&' +  // redundent?
     'sortOrder=PricePlusShippingLowest'
   ].join();
 
-  const socket = new WebSocket(url);
-  // Listen for messages asynchronously
-  socket.addEventListener('message', function(event) {
-    // interpret returned JSON
-    // const req = event.data;
-    if (gpu.data.findItemsAdvancedResponse[0].searchResult[0]['@count'] > 0) {
-      for (const i of gpu.data.findItemsAdvancedResponse[0]
-               .searchResult[0]
-               .item) {
-        // price = price + shipping cost
-        // shipping price cannot be determined if calculated method is used
-        if (i.shippingInfo[0].shippingServiceCost) {
-          price = i.sellingStatus[0].currentPrice[0].__value__ +
-              i.shippingInfo[0].shippingServiceCost[0].__value__;
-        } else {
-          price = i.sellingStatus[0].currentPrice[0].__value__;
-        }
 
-        // push data to postgres db
-        // clients will also use environment variables
-        // for connection information
-        addToDB(gpu, i, price);
+  await (async () => {
+    try {
+      // returns response
+      await got(url);
+      if (gpu.data.findItemsAdvancedResponse[0].searchResult[0]['@count'] > 0) {
+        for (const i of gpu.data.findItemsAdvancedResponse[0]
+                 .searchResult[0]
+                 .item) {
+          // price = price + shipping cost
+          // shipping price cannot be determined if calculated method is used
+          if (i.shippingInfo[0].shippingServiceCost) {
+            price = i.sellingStatus[0].currentPrice[0].__value__ +
+                i.shippingInfo[0].shippingServiceCost[0].__value__;
+          } else {
+            price = i.sellingStatus[0].currentPrice[0].__value__;
+          }
+          // push data to postgres db
+          // clients will also use environment variables
+          // for connection information
+          addToDB(gpu, i, price);
+        }
       }
+    } catch (error) {
+      console.log(error);
+      //=> 'Internal server error ...'
     }
-  });
+  })();
 }
 
 async function addToDB(gpu, i, price) {
