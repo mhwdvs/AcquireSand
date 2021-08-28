ed3f352ddc44cdf8dbd87232aac5f74a296022b3
Matthew Davis <matt@mhwdvs.com>
2021-08-28 15:29:15 +0800

    Make ebayapi a little bit less verbose

--- containers/ebayapi/index.js
+++ containers/ebayapi/index.js
@@ -10,69 +10,77 @@ pool.on('error', (err, client) => {
 loop();
 
 // database update loop
-async function loop(){
-  while(true){
-    await main();
-    console.log("Looping!");
+async function loop() {
+  while (true) {
+    try {
+      await main();
+    } catch (e) {
+      console.error(e);
+    }
+    sleep(5);
   }
 }
 
 async function main() {
   gpuList = [];
   // get all gpulist
-  console.log('Getting GPU list from database');
+  // Getting GPU list from database
   try {
     gpuList = await getGPUList();
-    console.log("final initial gpulist: " + gpuList);
+    console.log('final initial gpulist: ' + gpuList);
   } catch (e) {
-    console.error('Error in getting GPU list from database' + e.name + e.message);
+    console.error(
+        'Error in getting GPU list from database' + e.name + e.message);
     return;
   }
 
-  console.log('Setting existing listings as \"Unavailable\"');
+  // Setting existing listings as "Unavailable"
   try {
     await setExistingUnavailable();
   } catch (e) {
-    console.log('Error in setting existing listings as "Unavailable"' + e.name + e.message);
+    console.log(
+        'Error in setting existing listings as "Unavailable"' + e.name +
+        e.message);
     return;
   }
 
-  console.log('Getting new ebay listings and updating database');
+  // Getting new ebay listings and updating database
   try {
     await getListings(gpuList);
   } catch (e) {
-    console.log('Error in getting new ebay listings and updating database' + e.name + e.message);
+    console.log(
+        'Error in getting new ebay listings and updating database' + e.name +
+        e.message);
     return;
   }
 
-  console.log('Removing \"Unavailable\" listings');
+  // Removing "Unavailable" listings
   try {
     await purgeUnavailable();
   } catch (e) {
-    console.log('Error in removing "Unavailable" listings' + e.name + e.message);
+    console.log(
+        'Error in removing "Unavailable" listings' + e.name + e.message);
     return;
   }
 }
 
 async function getGPUList() {
   // get all gpulist
-  try{
-    console.log("Accessing database")
-    var gpulist = await(pool.query('SELECT * FROM gpulist'));
-    console.log("Got data")
-    if(gpulist.rows.length === 0){
-      console.error("Failed to get any GPUs");
-      throw(new Error("Failed to get any GPUs"));
+  try {
+    // Accessing database
+    var gpulist = await (pool.query('SELECT * FROM gpulist'));
+    // Got data
+    if (gpulist.rows.length === 0) {
+      throw (new Error('Failed to get any GPUs'));
     }
     var res = [];
-    for(var i = 0; i < gpulist.rows.length; ++i){
-      //replace spaces with + to be suitable for url request format
+    for (var i = 0; i < gpulist.rows.length; ++i) {
+      // replace spaces with + to be suitable for url request format
       res.push(gpulist.rows[i].name.split(' ').join('+'));
     }
-    console.log("Got gpu list!");
+    // Got gpu list!
     return res
-  }
-  catch(err){
+  } catch (err) {
     console.error('Error getting GPU List: ' + err.name + err.message);
     throw err;
   }
@@ -84,12 +92,14 @@ async function setExistingUnavailable() {
   // for connection information
   pool.connect((err, client, release) => {
     if (err) {
-      return console.error('Error acquiring client', err.stack);
+      throw (new Error('Error acquiring client: ' + err.stack));
     }
     client.query('UPDATE gpudb SET available = FALSE', (err, res) => {
       release();
-      if (err) return console.error('Error executing query', err.stack);
-    }); // has result if neccessary
+      if (err) {
+        throw (new Error('Error executing query: ' + err.stack));
+      }
+    });  // has result if neccessary
   });
 }
 
@@ -115,45 +125,41 @@ async function getGPU(gpu) {
 
   // make request to ebay api
   // Create WebSocket connection.
-  const url = ['https://svcs.ebay.com/services/search/FindingService/v1?',
-    'OPERATION-NAME=findItemsAdvanced&',
-    'SERVICE-VERSION=1.13.0&',
-    'SECURITY-APPNAME=',
-    process.env('EBAY_API_KEY'),
-    '&',
-    'RESPONSE-DATA-FORMAT=JSON&',
-    'REST-PAYLOAD=true&',
+  const url = [
+    'https://svcs.ebay.com/services/search/FindingService/v1?',
+    'OPERATION-NAME=findItemsAdvanced&', 'SERVICE-VERSION=1.13.0&',
+    'SECURITY-APPNAME=', process.env('EBAY_API_KEY'), '&',
+    'RESPONSE-DATA-FORMAT=JSON&', 'REST-PAYLOAD=true&',
     'GLOBAL-ID=EBAY-' + COUNTRY + '&',
-    'affiliate.networkId=9&', // SHOULD BE VARIABLE
-    'affiliate.trackingId=5338664158&', // SHOULD BE VARIABLE
+    'affiliate.networkId=9&',            // SHOULD BE VARIABLE
+    'affiliate.trackingId=5338664158&',  // SHOULD BE VARIABLE
     // ITEM FILTERS
     // additional item filters can be found at
     // https://developer.ebay.com/devzone/finding/callref/types/ItemFilterType.html
     'itemFilter(0).name=Condition&',
-    'itemFilter(0).value(0)=3000&', // Used
-    'itemFilter(0).value(1)=1000&', // New
-    'itemFilter(0).value(2)=1500&', // New (other, see item details
-    'itemFilter(0).value(3)=2000&', // Manufacturer refurbished
-    'itemFilter(0).value(4)=4000&', // Very good
-    'itemFilter(0).value(5)=5000&', // Good
-    'itemFilter(0).value(6)=6000&', // Acceptable
-    'itemFilter(1).name=ListingType&',
-    'itemFilter(1).value(0)=FixedPrice&',
+    'itemFilter(0).value(0)=3000&',  // Used
+    'itemFilter(0).value(1)=1000&',  // New
+    'itemFilter(0).value(2)=1500&',  // New (other, see item details
+    'itemFilter(0).value(3)=2000&',  // Manufacturer refurbished
+    'itemFilter(0).value(4)=4000&',  // Very good
+    'itemFilter(0).value(5)=5000&',  // Good
+    'itemFilter(0).value(6)=6000&',  // Acceptable
+    'itemFilter(1).name=ListingType&', 'itemFilter(1).value(0)=FixedPrice&',
     'itemFilter(1).value(1)=AuctionWithBIN&',
     'itemFilter(1).value(2)=StoreInventory&',
-    'itemFilter(2).name=LocalPickupOnly&',
-    'itemFilter(2).value=false&',
+    'itemFilter(2).name=LocalPickupOnly&', 'itemFilter(2).value=false&',
     'itemFilter(3).name=Currency&',
-    'itemFilter(3).value=' + CURRENCY + '&', // LOCALISATION TARGET
+    'itemFilter(3).value=' + CURRENCY + '&',  // LOCALISATION TARGET
     'itemFilter(4).name=LocatedIn&',
-    'itemFilter(4).value=' + COUNTRY + '&', // LOCALISATION TARGET
+    'itemFilter(4).value=' + COUNTRY + '&',  // LOCALISATION TARGET
     'keywords=',
-    gpu, // The gpu  being searched for
-    '+-cooler+-performance+-like+-backplate+-waterblock+-buying+-bracket+-fan+-fans+-replacement+-mosfet+-powerlink+-bios+-nvlink+-kit+-suits+-faulty+-1080p+-description&', // Blacklisted terms
-    'LH_PrefLoc=1&', // Only show items within region selected
+    gpu,  // The gpu  being searched for
+    '+-cooler+-performance+-like+-backplate+-waterblock+-buying+-bracket+-fan+-fans+-replacement+-mosfet+-powerlink+-bios+-nvlink+-kit+-suits+-faulty+-1080p+-description&',  // Blacklisted terms
+    'LH_PrefLoc=1&',  // Only show items within region selected
     'categoryId=27386&',
-    'paginationInput.entriesPerPage=100&', // redundent?
-    'sortOrder=PricePlusShippingLowest'].join();
+    'paginationInput.entriesPerPage=100&',  // redundent?
+    'sortOrder=PricePlusShippingLowest'
+  ].join();
 
   const socket = new WebSocket(url);
   // Listen for messages asynchronously
@@ -161,11 +167,14 @@ async function getGPU(gpu) {
     // interpret returned JSON
     // const req = event.data;
     if (gpu.data.findItemsAdvancedResponse[0].searchResult[0]['@count'] > 0) {
-      for (const i of gpu.data.findItemsAdvancedResponse[0].searchResult[0].item) {
+      for (const i of gpu.data.findItemsAdvancedResponse[0]
+               .searchResult[0]
+               .item) {
         // price = price + shipping cost
         // shipping price cannot be determined if calculated method is used
         if (i.shippingInfo[0].shippingServiceCost) {
-          price = i.sellingStatus[0].currentPrice[0].__value__ + i.shippingInfo[0].shippingServiceCost[0].__value__;
+          price = i.sellingStatus[0].currentPrice[0].__value__ +
+              i.shippingInfo[0].shippingServiceCost[0].__value__;
         } else {
           price = i.sellingStatus[0].currentPrice[0].__value__;
         }
@@ -185,19 +194,29 @@ async function addToDB(gpu, i, price) {
     if (err) {
       return console.error('Error acquiring client', err.stack);
     }
-    client.query('IF \
+    client.query(
+        'IF \
       (EXISTS\
-      (SELECT * FROM gpudb WHERE gpu = ' + gpu + ' AND title = ' + i.title +' AND itemurl = ' + i.viewItemURL + ' AND imageurl = ' + i.galleryURL + ' AND price = ' + price + '))\
+      (SELECT * FROM gpudb WHERE gpu = ' +
+            gpu + ' AND title = ' + i.title +
+            ' AND itemurl = ' + i.viewItemURL +
+            ' AND imageurl = ' + i.galleryURL + ' AND price = ' + price + '))\
       BEGIN \
-      UPDATE gpudb SET available TRUE WHERE WHERE gpu = ' + gpu + ' AND title = ' + i.title +' AND itemurl = ' + i.viewItemURL + ' AND imageurl = ' + i.galleryURL + ' AND price = ' + price + '\
+      UPDATE gpudb SET available TRUE WHERE WHERE gpu = ' +
+            gpu + ' AND title = ' + i.title +
+            ' AND itemurl = ' + i.viewItemURL +
+            ' AND imageurl = ' + i.galleryURL + ' AND price = ' + price + '\
       END \
       ELSE \
       BEGIN \
-      INSERT INTO gpudb (gpu, title, itemurl, imageurl, price, currency, available) VALUES (' + gpu + ', ' + i.title + ', ' + i.viewItemURL + ', ' + i.galleryURL + ', ' + price + ', ' + CURRENCY + ', TRUE)\
-      END', (err, res) => {
-      release();
-      if (err) return console.error('Error executing query', err.stack);
-    });
+      INSERT INTO gpudb (gpu, title, itemurl, imageurl, price, currency, available) VALUES (' +
+            gpu + ', ' + i.title + ', ' + i.viewItemURL + ', ' + i.galleryURL +
+            ', ' + price + ', ' + CURRENCY + ', TRUE)\
+      END',
+        (err, res) => {
+          release();
+          if (err) return console.error('Error executing query', err.stack);
+        });
   });
 }
 
@@ -210,12 +229,14 @@ async function purgeUnavailable() {
     if (err) {
       return console.error('Error acquiring client', err.stack);
     }
-    client.query('DELETE FROM gpudb \
+    client.query(
+        'DELETE FROM gpudb \
       WHERE (available, FALSE) IN \
-      ( SELECT available, FALSE FROM gpudb )', (err, res) => {
-      release();
-      if (err) return console.error('Error executing query', err.stack);
-    });
+      ( SELECT available, FALSE FROM gpudb )',
+        (err, res) => {
+          release();
+          if (err) return console.error('Error executing query', err.stack);
+        });
   });
   return;
 }
