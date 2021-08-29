83370160a3c21ab11c1c62a3db584dd4e3477240
Matthew Davis <matt@mhwdvs.com>
2021-08-29 23:05:24 +0800

    Remove available column

--- containers/postgres-init/run.sh
+++ containers/postgres-init/run.sh
@@ -10,13 +10,13 @@ psql -U ${PGUSER} -h ${PGHOST} -c "drop table lastupdate;"
 
 # create tables if they don't already exist
 psql -U ${PGUSER} -h ${PGHOST} -c "create table gpulist(name TEXT, relative DOUBLE PRECISION, brand TEXT, PRIMARY KEY (name));"
-psql -U ${PGUSER} -h ${PGHOST} -c "create table gpudb(title TEXT, gpu TEXT, itemurl TEXT, imageurl TEXT, price MONEY, currency TEXT, available BOOLEAN, PRIMARY KEY (itemurl), CONSTRAINT fk_gpuname FOREIGN KEY(gpu) REFERENCES gpulist(name));"
+psql -U ${PGUSER} -h ${PGHOST} -c "create table gpudb(title TEXT, gpu TEXT, itemurl TEXT, imageurl TEXT, price MONEY, currency TEXT, PRIMARY KEY (itemurl), CONSTRAINT fk_gpuname FOREIGN KEY(gpu) REFERENCES gpulist(name));"
 psql -U ${PGUSER} -h ${PGHOST} -c "create table lastupdate();"
 
 sleep 5
 
 # create a dummy gpudb entry
-psql -U ${PGUSER} -h ${PGHOST} -c "insert into gpudb (title, gpu, itemurl, imageurl, price, currency, available) values ('Test GPU!', (select name from gpulist where name='NVIDIA GeForce RTX 3080'), 'https://www.youtube.com/watch?v=8ZtW1ziF9u0', 'https://cdn.frankerfacez.com/emoticon/130077/4', 777.77, 'AUD', true);"
+psql -U ${PGUSER} -h ${PGHOST} -c "insert into gpudb (title, gpu, itemurl, imageurl, price, currency, available) values ('Test GPU!', (select name from gpulist where name='NVIDIA GeForce RTX 3080'), 'https://www.youtube.com/watch?v=8ZtW1ziF9u0', 'https://cdn.frankerfacez.com/emoticon/130077/4', 777.77, 'AUD');"
 
 # display tables in database
 while true 
