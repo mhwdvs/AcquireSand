2d17131dec6b51aeab372a7016e3e2891a5a74b3
Matthew Davis <matt@mhwdvs.com>
2021-08-29 23:01:24 +0800

    IT WORKS

--- containers/angular/src/app/components/gpu-search/gpu-search.component.ts
+++ containers/angular/src/app/components/gpu-search/gpu-search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-gpu-search',
@@ -24,25 +25,22 @@ export class GpuSearchComponent implements OnInit {
   // temp
   original_data = [];
 
-  constructor(private http:HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     // get gpu list
-    this.http.get(environment.get_gpus_endpoint,{
-    })
-      .subscribe(
-        (val: any) => {
-          console.log("Post call successful value returned in body", val);
-          this.original_data = val;
-        },
-        response => {
-          console.log("POST call in error", response);
-        },
-        () => {
-          console.log("The POST observable is now completed.");
-        }
-      );
+    this.http.get(environment.get_gpus_endpoint, {})
+        .subscribe(
+            (val: any) => {
+              console.log('Post call successful value returned in body', val);
+              this.original_data = val;
+            },
+            response => {
+              console.log('POST call in error', response);
+            },
+            () => {
+              console.log('The POST observable is now completed.');
+            });
     // get initial listings
     this.add_listings(0, this.listing_batch_size);
   }
@@ -59,78 +57,80 @@ export class GpuSearchComponent implements OnInit {
     this.next_listing_index += this.listing_batch_size - 1;
   }
 
-  // filters and pushes 
-  add_listings(first: number, required: number){
+  // filters and pushes
+  add_listings(first: number, required: number) {
     // get next batch of gpus
-    let data;
     // get listings from API
-    this.http.post(environment.get_listings_endpoint,{
-      // request body
-      "filters": this.currentfilters,
-      "count": this.listing_batch_size,
-      "first": first
-    })
-      .subscribe(
-        (val) => {
-          console.log("Post call successful value returned in body", val);
-          data = val;
-          this.next_listing_index += data.match_count;
-          for(let i of data.matches){
-            this.gpus.push(i);
-          }
-          if(first == 0){
-            // update last_updated, Unix epoch in seconds to milliseconds (*1000)
-            this.last_update = new Date(data.outtime  * 1000);
-          }
+    this.http
+        .post(environment.get_listings_endpoint, {
+          // request body
+          'filters': this.currentfilters,
+          'count': this.listing_batch_size,
+          'first': first
+        })
+        .subscribe(
+            (val) => {
+              console.log('Post call successful value returned in body', val);
+              // this.next_listing_index += val.match_count;
+              for (let i of val.matches.rows) {
+                this.gpus.push(i);
+              }
+              if (first == 0) {
+                // update last_updated, Unix epoch in seconds to milliseconds
+                // (*1000)
+                this.last_update = new Date(val.outtime * 1000);
+              }
 
-          if(data.end_of_listings){
-            this.end_of_results = true;
-            this.no_more_results = true;
-            this.no_results = false;
-          }
-          else if(data.no_matches){
-            this.end_of_results = true;
-            this.no_more_results = false;
-            this.no_results = true;
-          }
-          else {
-            this.end_of_results = false;
-            this.no_more_results = false;
-            this.no_results = false;
-          }
-        },
-        response => {
-          console.log("POST call in error", response);
-        },
-        () => {
-          console.log("The POST observable is now completed.");
-          this.adding_listings = false;
-        }
-      );
+              /*
+              if(val.end_of_listings){
+                this.end_of_results = true;
+                this.no_more_results = true;
+                this.no_results = false;
+              }
+              else if(val.no_matches){
+                this.end_of_results = true;
+                this.no_more_results = false;
+                this.no_results = true;
+              }
+              else {
+                this.end_of_results = false;
+                this.no_more_results = false;
+                this.no_results = false;
+              }*/
+            },
+            response => {
+              console.log('POST call in error', response);
+            },
+            () => {
+              console.log('The POST observable is now completed.');
+              this.adding_listings = false;
+            });
   }
 
-  scroll_top(){
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  scroll_top() {
+    document.body.scrollTop = 0;  // For Safari
+    document.documentElement.scrollTop =
+        0;  // For Chrome, Firefox, IE and Opera
   }
 
-  @HostListener('window:scroll', ['$event']) scroll_event(){
+  @HostListener('window:scroll', ['$event'])
+  scroll_event() {
     let anchorpos = this.anchor.nativeElement.getBoundingClientRect()
-    let topBtn = document.getElementById("topBtn");
+    let topBtn = document.getElementById('topBtn');
     // check if anchor is in view
-    if(anchorpos.top < window.innerHeight){
+    if (anchorpos.top < window.innerHeight) {
       // anchor is in view, add gpus to dom object
-      if(this.adding_listings == false){
+      if (this.adding_listings == false) {
         this.adding_listings = true;
         this.add_listings(this.next_listing_index, this.listing_batch_size);
-      }      
+      }
     }
     // Check if top is out of view
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-      topBtn.style.display = "block";
-    }
-    else{
-      topBtn.style.display = "none";
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+      topBtn.style.display = 'block';
+    } else {
+      topBtn.style.display = 'none';
     }
   }
 }
--- containers/postgres-init/run.sh
+++ containers/postgres-init/run.sh
@@ -16,7 +16,7 @@ psql -U ${PGUSER} -h ${PGHOST} -c "create table lastupdate();"
 sleep 5
 
 # create a dummy gpudb entry
-psql -U ${PGUSER} -h ${PGHOST} -c "insert into gpudb (title, gpu, itemurl, imageurl, price, currency, available) values ('Test GPU!', (select name from gpulist where name='NVIDIA GeForce RTX 3080'), 'https://i.ytimg.com/an_webp/8ZtW1ziF9u0/mqdefault_6s.webp?du=3000&sqp=CPSeqIkG&rs=AOn4CLBEymM6gwmpYRwYrQrN6T2mgwud_A', 'https://cdn.frankerfacez.com/emoticon/130077/4', 777.77, 'AUD', true);"
+psql -U ${PGUSER} -h ${PGHOST} -c "insert into gpudb (title, gpu, itemurl, imageurl, price, currency, available) values ('Test GPU!', (select name from gpulist where name='NVIDIA GeForce RTX 3080'), 'https://www.youtube.com/watch?v=8ZtW1ziF9u0', 'https://cdn.frankerfacez.com/emoticon/130077/4', 777.77, 'AUD', true);"
 
 # display tables in database
 while true 
