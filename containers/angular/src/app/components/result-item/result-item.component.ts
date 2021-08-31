dd59a3edcc4fd432bb978c3a045cc29c5d95526b
Matthew Davis <matt@mhwdvs.com>
2021-08-31 20:25:21 +0800

    Remove graphs from listings

--- containers/angular/src/app/components/result-item/result-item.component.ts
+++ containers/angular/src/app/components/result-item/result-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-result-item',
@@ -6,33 +6,14 @@ import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./result-item.component.css']
 })
 export class ResultItemComponent {
-  @Input() gpu:any;
+  @Input() gpu: any;
   @ViewChild('dial') dial_el: ElementRef;
 
   degToRad(degrees) {
     return degrees * Math.PI / 180;
   };
 
-  ngOnInit() {
-    console.log("making graph!");
-    
-  }
+  ngOnInit() {}
 
-  ngAfterViewInit(){
-    // unsure if this searches component or whole page
-    const canvas = this.dial_el.nativeElement;
-
-    canvas.height = 60;
-    canvas.width = 100;
-    const ctx = canvas.getContext('2d');
-    let ppp = this.gpu.ppp;
-
-    // price
-    ctx.beginPath();
-    ctx.moveTo(20,55);
-    ctx.arc(50, 55, 45, this.degToRad(-180), this.degToRad(-80), false);
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 8;
-    ctx.stroke(); 
-  }
+  ngAfterViewInit() {}
 }
