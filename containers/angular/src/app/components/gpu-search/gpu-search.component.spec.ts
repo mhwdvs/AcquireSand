5c9fe84dfb0d516c8f00eb115ca22881a3a8d5c1
Matthew Davis <matt@mhwdvs.com>
2021-10-10 17:19:27 +0000

    Update angular to 11

--- containers/angular/src/app/components/gpu-search/gpu-search.component.spec.ts
+++ containers/angular/src/app/components/gpu-search/gpu-search.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GpuSearchComponent } from './gpu-search.component';
 
@@ -6,7 +6,7 @@ describe('SearchFiltersComponent', () => {
   let component: GpuSearchComponent;
   let fixture: ComponentFixture<GpuSearchComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GpuSearchComponent ]
     })
--- containers/angular/src/app/components/result-item/result-item.component.spec.ts
+++ containers/angular/src/app/components/result-item/result-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ResultItemComponent } from './result-item.component';
 
@@ -6,7 +6,7 @@ describe('ResultItemComponent', () => {
   let component: ResultItemComponent;
   let fixture: ComponentFixture<ResultItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ResultItemComponent ]
     })
