b84e85db8d40426e9d6281a13617dd85c2533277
matt <mhwdvs@gmail.com>
2021-10-10 19:05:09 +0800

    Remove port specification from frontend

--- containers/angular/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  get_gpus_endpoint: 'https://gpu.mhwdvs.com:60777/get_gpus',
-  get_listings_endpoint: "https://gpu.mhwdvs.com:60777/get_listings"
-};
